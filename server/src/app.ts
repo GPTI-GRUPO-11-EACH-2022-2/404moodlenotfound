a585409549f7d2501075e8cba2dde137d552be36
Emanuel Tavares <enriqueemanuelcontato@gmail.com>
2022-10-14 19:49:52 -0300

    fix(server): Change order of expressions

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,10 @@ import express, { Application, Request, Response, NextFunction } from 'express';
 const port = process.env.PORT || 5000
 const app: Application = express();
 
+app.listen(port);
+
+console.log('Server running on port: ', port)
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
@@ -10,5 +14,3 @@ app.use(express.json());
 app.post('/api/', (req: Request, res: Response) => {
     res.send("Esta é uma " + req.body.title + " a mensagem é: " + req.body.messageBody);
 });
-
-app.listen(port, () => console.log('Server running'));
