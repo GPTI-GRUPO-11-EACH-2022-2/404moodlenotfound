b16fd74446c34245f28f5a636150bf74b64d5092
Emanuel Tavares <enriqueemanuelcontato@gmail.com>
2022-10-14 14:23:41 -0300

    fix: Make entrypoint run quicker

--- Dockerfile
+++ Dockerfile
@@ -12,6 +12,10 @@ COPY ./client/ /miglia/client/
 COPY entrypoint.sh /usr/bin/
 RUN chmod +x /usr/bin/entrypoint.sh
 
+RUN cd /miglia/server && npm install 
+
+RUN cd /miglia/client && npm install 
+
 EXPOSE 80
 EXPOSE 5000
 ENTRYPOINT ["entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 WORKDIR=$(pwd)
-cd $WORKDIR/server && npm install && npm run dev 2>&1 > server.log &
-cd $WORKDIR/client && npm install && npm start --port $PORT 2>&1 > client.log
+cd $WORKDIR/server && npm run dev 2>&1 > server.log &
+cd $WORKDIR/client && npm start --port $PORT 2>&1 > client.log
