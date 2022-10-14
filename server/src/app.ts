c4bafa2f7967550ea84a941afdecb2cad078ba54
Emanuel Tavares <enriqueemanuelcontato@gmail.com>
2022-10-14 19:58:31 -0300

    fix(server): Adding debug to server initialization

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,13 +3,11 @@ import express, { Application, Request, Response, NextFunction } from 'express';
 const port = process.env.PORT || 5000
 const app: Application = express();
 
+app.listen(port, () => console.log('Server running on port: ', port));
+
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
-
-app.listen(port);
-
-console.log('Server running on port: ', port)
 
 app.post('/api/', (req: Request, res: Response) => {
     res.send("Esta é uma " + req.body.title + " a mensagem é: " + req.body.messageBody);
