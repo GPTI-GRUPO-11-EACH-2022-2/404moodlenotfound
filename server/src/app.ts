79108c102524922804ebe583e9a1318b0e805a81
Emanuel Tavares <enriqueemanuelcontato@gmail.com>
2022-10-14 18:03:09 -0300

    Make server listen to port env var

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
 
+const port = process.env.port || 5000
 const app: Application = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -10,4 +11,4 @@ app.post('/api/', (req: Request, res: Response) => {
     res.send("Esta é uma " + req.body.title + " a mensagem é: " + req.body.messageBody);
 });
 
-app.listen(5000, () => console.log('Server running'));
+app.listen(port, () => console.log('Server running'));
